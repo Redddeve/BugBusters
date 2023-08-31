4b67d8b2612d04c90e08583da7883c7ea7e6a4a7
DianaKryzhanivska <di_parkhomenko@ukr.net>
2023-08-31 12:43:56 +0300

    refactoring code hero.js

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,62 +1,60 @@
+const select = document.getElementById('heroSelect');
+const container = document.querySelector('.hero-select-items');
+
+renderSelectHero();
+renderButtonsHero();
+
 // ====================== MOBILE ========================
 
-const select = document.getElementById('heroSelect');
+function renderSelectHero() {
+  // цикл для додавання у select букв A - Z по Unicode
+  for (let i = 65; i <= 90; i++) {
+    templateSelectHero(i);
+  }
 
-// цикл для додавання у select букв англ. алф. від A до Z по Unicode
-for (let i = 65; i <= 90; i++) {
+  // цикл для додавання у select цифр 1 - 9 по Unicode
+  for (let i = 49; i <= 57; i++) {
+    templateSelectHero(i);
+  }
+
+  // додавання у select цифри 0 по Unicode
+  templateSelectHero(48);
+}
+
+function templateSelectHero(i) {
   const letter = String.fromCharCode(i);
   const option = document.createElement('option');
   option.text = letter;
   option.value = letter;
   select.appendChild(option);
 }
-
-// цикл для додавання у select цифр від 1 до 9 по Unicode
-for (let i = 49; i <= 57; i++) {
-  const letter = String.fromCharCode(i);
-  const option = document.createElement('option');
-  option.text = letter;
-  option.value = letter;
-  select.appendChild(option);
-}
-
-// додавання у select цифри 0 по Unicode
-const letter = String.fromCharCode(48);
-const option = document.createElement('option');
-option.text = letter;
-option.value = letter;
-select.appendChild(option);
 
 // ================== TABLET & DESKTOP ====================
 
-const container = document.querySelector('.hero-select-items');
+function renderButtonsHero() {
+  // цикл для додавання у select букв букв A - Z по Unicode
+  for (let i = 65; i <= 90; i++) {
+    templateButtonsHero(i);
+  }
 
-// цикл для додавання у select букв англ. алф. від A до Z по Unicode
-for (let i = 65; i <= 90; i++) {
+  // цикл для додавання у container цифр 1 - 9 по Unicode
+  for (let i = 49; i <= 57; i++) {
+    templateButtonsHero(i);
+  }
+
+  // додавання у container цифри 0 по Unicode
+  templateButtonsHero(48);
+}
+
+function templateButtonsHero(i) {
   // створюємо кнопку
   const button = document.createElement('button');
-
-  // задаємо текст для кнопки - букву алфавіту
+  // задаємо текст для кнопки - букву алфавіту/цифру
   button.textContent = String.fromCharCode(i);
-
   // додаємо кнопку до контейнера
   container.appendChild(button);
-
   button.classList.add('hero-select-btn');
+  button.setAttribute('type', 'button');
 }
-
-// цикл для додавання у container цифр від 1 до 9 по Unicode
-for (let i = 49; i <= 57; i++) {
-  const button = document.createElement('button');
-  button.textContent = String.fromCharCode(i);
-  container.appendChild(button);
-  button.classList.add('hero-select-btn');
-}
-
-// додавання у select цифри 0 по Unicode
-const button = document.createElement('button');
-button.textContent = String.fromCharCode(48);
-container.appendChild(button);
-button.classList.add('hero-select-btn');
 
 //==========================================================
