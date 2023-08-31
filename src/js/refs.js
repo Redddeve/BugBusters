679bd92b92f293ccf73c831291bd01db2162b36f
Redddeve <popko.dmitry797@gmail.com>
2023-08-31 12:41:45 +0300

    Completed header
    Needs some JS tweaking

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,9 +1,10 @@
-// const refs = {
-
-// toggleListButtonUp: document.querySelector('button-for-favorite-up'),
-// toggleListButtonDown: document.querySelector('button-for-favorite-down'),
-// favList: document.querySelector('fav-open-list')
-
-// } 
-
-// export {refs}
+export const refs = {
+  toggleListButtonUp: document.querySelector('.button-for-favorite-up'),
+  toggleListButtonDown: document.querySelector('.button-for-favorite-down'),
+  favList: document.querySelector('.fav-open-list'),
+  favLink: document.querySelector('.fav-link-button'),
+  favBtn: document.querySelector('.header-fav-toggle p'),
+  favArrowD: document.querySelector('.fav-arrow__down'),
+  favArrowU: document.querySelector('.fav-arrow__up'),
+  favBlock: document.querySelector('.fav-block'),
+};
