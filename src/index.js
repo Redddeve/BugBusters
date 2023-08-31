728151924cec4f40405c9e354697279a6a6e33ad
Redddeve <popko.dmitry797@gmail.com>
2023-08-31 21:08:07 +0300

    File tweaks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './js/header.js';
 import './js/hero.js';
+import './js/cocktail-fav-card-render.js';
 import './js/onLoad.js';
-import './js/cocktail-main-card-render.js';
 import './js/modal-cocktails.js';
