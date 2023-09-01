56eac3e0ccfceecb9ca343cc5a0ab547b54236f4
kulikiliya <136320307+kulikiliya@users.noreply.github.com>
2023-09-01 16:02:25 +0300

    add-to-fav-function
    Функция для добавления/убрать из favorites
    И немного обновил murkup для главной страницы

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './js/hero.js';
 import './js/cocktail-fav-card-render.js';
 import './js/onLoad.js';
 import './js/modal-cocktails.js';
+import './js/storage.js'
 import './js/modal-ingredients.js';
