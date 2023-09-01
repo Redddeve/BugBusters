36bcce47196cd611df21cb22ef28522d07d81917
Redddeve <popko.dmitry797@gmail.com>
2023-09-01 09:55:48 +0300

    Update onLoad.js

--- src/js/onLoad.js
+++ src/js/onLoad.js
@@ -1,12 +1,13 @@
 import { fetchRandomCocktails } from './fetch-data.js';
+import { cocktailMainCardRender } from './cocktail-fav-card-render.js';
 
 const onLoad = async () => {
   if (window.innerWidth >= 1280) {
     let data = await fetchRandomCocktails(9);
-    renderCards(data);
+    cocktailMainCardRender(data);
   } else {
     let data = await fetchRandomCocktails(8);
-    renderCards(data);
+    cocktailMainCardRender(data);
   }
 };
 
