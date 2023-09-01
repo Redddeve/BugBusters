73a89dfea93176c6576f0d75f33465f6d5f3cb01
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-09-01 20:09:33 +0300

    +

---
+++ src/js/pagination.js
@@ -0,0 +1,17 @@
+// import { refs } from refs;
+
+// const refs = {
+//   leftPagBtn: document.querySelector('.left-pag-btn'),
+//   rightPagBtn: document.querySelector('.right-pag-btn'),
+//   fifthBtn: document.querySelector('.page-num-5'),
+//   sixthBtn: document.querySelector('.page-num-6'),
+//   seventhBtn: document.querySelector('.page-num-7'),
+// };
+
+// refs.fifthBtn.classList.add('visually-hidden');
+// refs.sixthBtn.classList.add('visually-hidden');
+// refs.seventhBtn.classList.add('visually-hidden');
+
+// refs.fifthBtn.classList.remove('visually-hidden');
+// refs.sixthBtn.classList.remove('visually-hidden');
+// refs.seventhBtn.classList.remove('visually-hidden');
