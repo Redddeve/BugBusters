4a89a9c03ed0cd16702ff207b7b443b6c31bc6ac
Disclo5ure <46845468m@gmail.com>
2023-09-02 21:27:15 +0300

    everything at once

--- src/js/local-storage-operations.js
+++ src/js/local-storage-operations.js
@@ -1,12 +1,14 @@
 export const addToLS = (type, id) => {
-  const arrayOfIds = JSON.parse(localStorage.getItem(type));
-  arrayOfIds.push(id);
-  localStorage.setItem(type, JSON.stringify(arrayOfIds));
+  if (!localStorage.getItem(type)) localStorage.setItem(type, id);
+  const arrayOfIds = localStorage.getItem(type);
+  if (!arrayOfIds.includes(id)) {
+    arrayOfIds.push(id);
+    localStorage.setItem(type, JSON.stringify(arrayOfIds));
+  } else return;
 };
 
 export const removeFromLS = (type, id) => {
   const arrayOfIds = JSON.parse(localStorage.getItem(type));
-  console.log(arrayOfIds);
   const idIndex = arrayOfIds.findIndex(item => item === id);
   arrayOfIds.splice(idIndex, 1);
   localStorage.setItem(type, JSON.stringify(arrayOfIds));
