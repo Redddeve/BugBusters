835bc68146a4e02026a10b54141959f975b32260
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-09-03 13:58:43 +0300

    new ver from TL

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,8 +2,20 @@ import { refs } from './refs.js';
 import { cocktailMainCardRender } from './cocktail-fav-card-render.js';
 
 export function renderPagination(cocktailArr) {
+  refs.paginationNumberBtnsContainer.innerHTML = '';
   let cardsPerPage;
 
+  let pageBtns = [];
+  let start = [];
+  let finish = [];
+
+  function isMobile() {
+    if (window.innerWidth >= 768) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   if (window.innerWidth >= 1280) {
     cardsPerPage = 9;
   } else {
@@ -13,28 +25,16 @@ export function renderPagination(cocktailArr) {
   let totalCocktailCount = cocktailArr.length;
 
   if (totalCocktailCount <= cardsPerPage) {
-    // refs.paginationContainer.classList.add('is-hidden');
+    refs.paginationContainer.classList.add('is-hidden');
   }
 
   let totalPagesNum = Math.ceil(totalCocktailCount / cardsPerPage);
 
   const sortedCardsArr = createPagesArr(cocktailArr, cardsPerPage);
 
-  cocktailMainCardRender(sortedCardsArr[0]);
-
-  refs.paginationNumberBtnsContainer.addEventListener(
-    'click',
-    onPaginationBtnClick
-  );
-  refs.leftPagBtn[0].addEventListener('click', onPaginationBtnClick);
-  refs.rightPagBtn[0].addEventListener('click', onPaginationBtnClick);
-
-  let currentPageIndex = 0;
-
-  // ======== func creating array of pages by tab+desk or mob cards per page number =====
-  function createPagesArr(arr, cardsPerPageNun) {
+  function createPagesArr(arr, cardsPerPageNum) {
     return arr.reduce((result, item, index) => {
-      if (index % cardsPerPageNun === 0) {
+      if (index % cardsPerPageNum === 0) {
         result.push([]);
       }
       result[result.length - 1].push(item);
@@ -42,64 +42,100 @@ export function renderPagination(cocktailArr) {
     }, []);
   }
 
-  // ========= func creating pagination buttons =================
-  function createPaginationBtns(totalDisplay) {
-    const btnsSample = ['1', '2'];
+  cocktailMainCardRender(sortedCardsArr[0]);
 
-    //   const
+  refs.leftPagBtn[0].addEventListener('click', onPaginationBtnClick);
+  refs.rightPagBtn[0].addEventListener('click', onPaginationBtnClick);
+  refs.paginationNumberBtnsContainer.addEventListener(
+    'click',
+    onPaginationBtnClick
+  );
 
-    // let markup = '';
-    // for (let index = startPageNum; index <= totalDisplay; index++) {
-    //   markup += `<button
-    //   class="page-num-${index + 1} pagination-button-item"
-    //   type="button"
-    //   data-action="${index + 1}"
-    // >
-    //   ${index + 1}
-    // </button>`;
-    // }
-    // console.log(markup);
-    refs.paginationNumberBtnsContainer.innerHTML = markup;
+  let currentPageIndex = 0;
+
+  for (let i = 1; i <= totalPagesNum; i++) {
+    const pageBtn = document.createElement('button');
+    pageBtn.textContent = i;
+    pageBtn.setAttribute('data-action', i);
+    pageBtn.classList.add('pagination-button-item');
+    pageBtn.addEventListener('click', onPaginationBtnClick);
+    pageBtns.push(pageBtn);
   }
 
-  // ============== pagination button click func ================
-  function onPaginationBtnClick(evt) {
-    // evt.preventDefault();
+  switch (isMobile()) {
+    case true:
+      {
+        console.log('is mobile');
+        start = pageBtns.slice(0, 2);
+        finish = pageBtns.slice(pageBtns.length - 1, pageBtns.length);
 
-    // const toSearch = document.getElementById('search');
-    // toSearch.scrollIntoView({ behavior: 'smooth' }, true);
+        const moreBtn = document.createElement('button');
+        moreBtn.textContent = '...';
+        moreBtn.classList.add('pagination-button-item');
+
+        refs.paginationNumberBtnsContainer.append(...start, moreBtn, ...finish);
+      }
+      break;
+
+    case false: {
+      console.log('is not mobile');
+      start = pageBtns.slice(0, 2);
+      finish = pageBtns.slice(pageBtns.length - 1, pageBtns.length);
+
+      const moreBtn = document.createElement('button');
+      moreBtn.textContent = '...';
+      moreBtn.classList.add('pagination-button-item');
+
+      refs.paginationNumberBtnsContainer.append(...start, moreBtn, ...finish);
+      break;
+    }
+  }
+
+  function onPaginationBtnClick(evt) {
+    const toSearch = document.getElementById('search');
+    toSearch.scrollIntoView({ behavior: 'smooth' }, true);
 
     let btnValue = evt.target.dataset.action;
 
-    let totalBtnsDisplay = 7;
-
     refs.mainCocktailsGallery.innerHTML = '';
+
+    if (refs.mainCocktailsText.textContent !== 'Searching results') {
+      refs.mainCocktailsText.textContent = 'Searching results';
+    }
 
     if (btnValue === 'leftPag') {
       currentPageIndex -= 1;
       if (currentPageIndex < 0) {
         currentPageIndex = 0;
-        refs.rightPagBtn[0].classlist.add('pagination-button-disabled');
-        refs.rightPagBtn[0].setAttribute.disabled;
       }
       cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
-      //   createPaginationBtns(currentPageIndex, totalBtnsDisplay);
     }
 
     if (btnValue === 'rightPag') {
       currentPageIndex += 1;
-      if (currentPageIndex > totalPagesNum - 1) {
-        currentPageIndex = totalPagesNum - 1;
-        refs.rightPagBtn[0].classlist.add('pagination-button-disabled');
-        refs.rightPagBtn[0].setAttribute.disabled;
+      if (currentPageIndex === totalPagesNum - 1) {
+        console.log(currentPageIndex);
+        console.log(totalPagesNum);
+        refs.rightPagBtn[0].setAttribute('disabled', '');
       }
+
       cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
-      //   createPaginationBtns(currentPageIndex, totalBtnsDisplay);
     }
 
     if (!isNaN(Number(btnValue))) {
       currentPageIndex = Number(btnValue - 1);
       cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
+    }
+
+    if (currentPageIndex === 0) {
+      refs.leftPagBtn[0].setAttribute('disabled', '');
+    } else {
+      refs.leftPagBtn[0].removeAttribute('disabled');
+    }
+    if (currentPageIndex === totalPagesNum - 1) {
+      refs.rightPagBtn[0].setAttribute('disabled', '');
+    } else {
+      refs.rightPagBtn[0].removeAttribute('disabled');
     }
   }
 }
