714a465f4dc50505907033de168e7b4e9fd3be5f
greenleaf-glitch <133708156+greenleaf-glitch@users.noreply.github.com>
2023-09-03 21:52:54 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,28 +2,8 @@ import { refs } from './refs.js';
 import { cocktailMainCardRender } from './cocktail-fav-card-render.js';
 
 export function renderPagination(cocktailArr) {
+  refs.paginationNumberBtnsContainer.innerHTML = '';
   let cardsPerPage;
-  let pageBtns = [];
-  let start = [];
-  let finish = [];
-  let totalCocktailCount = cocktailArr.length;
-  let currentPageIndex = 0;
-
-  // ========== more "..." btn sec===========
-  const moreBtn = document.createElement('button');
-  moreBtn.textContent = '...';
-  moreBtn.classList.add('pagination-button-item');
-  moreBtn.classList.add('is-hidden');
-  let moreBtnIndex = 4;
-
-  // ============== device type detection =========
-  function isMobile() {
-    if (window.innerWidth >= 768) {
-      return false;
-    } else {
-      return true;
-    }
-  }
 
   let currentPageIndex = 0;
   let pageBtns = [];
@@ -43,10 +23,7 @@ export function renderPagination(cocktailArr) {
     cardsPerPage = 8;
   }
 
-  // ================== create pages from data sec ====================
-  let totalPagesNum = Math.ceil(totalCocktailCount / cardsPerPage);
-
-  const sortedCardsArr = createPagesArr(cocktailArr, cardsPerPage);
+  let totalCocktailCount = cocktailArr.length;
 
   if (totalCocktailCount <= cardsPerPage) {
     refs.paginationContainer.classList.add('is-hidden');
@@ -56,7 +33,9 @@ export function renderPagination(cocktailArr) {
 
   const sortedCardsArr = createPagesArr(cocktailArr, cardsPerPage);
 
-  function createPagesArr(arr, cardsPerPageNun) {
+  arrowCheck();
+
+  function createPagesArr(arr, cardsPerPageNum) {
     return arr.reduce((result, item, index) => {
       if (index % cardsPerPageNum === 0) {
         result.push([]);
@@ -68,22 +47,65 @@ export function renderPagination(cocktailArr) {
 
   cocktailMainCardRender(sortedCardsArr[0]);
 
+  refs.leftPagBtn[0].addEventListener('click', onPaginationBtnClick);
+  refs.rightPagBtn[0].addEventListener('click', onPaginationBtnClick);
   refs.paginationNumberBtnsContainer.addEventListener(
     'click',
     onPaginationBtnClick
   );
 
-  refs.leftPagBtn[0].addEventListener('click', onPaginationBtnClick);
-  //   refs.leftPagBtn[1].addEventListener('click', onPaginationBtnClick);
-  refs.rightPagBtn[0].addEventListener('click', onPaginationBtnClick);
-  //   refs.rightPagBtn[1].addEventListener('click', onPaginationBtnClick);
+  for (let i = 1; i <= totalPagesNum; i++) {
+    const pageBtn = document.createElement('button');
+    pageBtn.textContent = i;
+    pageBtn.setAttribute('data-action', i);
+    pageBtn.classList.add('pagination-button-item');
+    pageBtn.addEventListener('click', onPaginationBtnClick);
+    pageBtns.push(pageBtn);
+  }
 
-  let currentPageIndex = 0;
+  switch (isMobile()) {
+    case true:
+      {
+        console.log('is mobile');
+        if (pageBtns.length <= 4) {
+          refs.paginationNumberBtnsContainer.append(...pageBtns);
+        } else {
+          start = pageBtns.slice(0, 3);
+          finish = pageBtns.slice(pageBtns.length - 1, pageBtns.length);
 
-  // ==================== on pagination click func ============
+          const moreBtn = document.createElement('button');
+          moreBtn.textContent = '...';
+          moreBtn.classList.add('pagination-button-item');
+
+          refs.paginationNumberBtnsContainer.append(
+            ...start,
+            moreBtn,
+            ...finish
+          );
+        }
+      }
+      break;
+
+    case false: {
+      console.log('is not mobile');
+      if (pageBtns.length <= 7) {
+        refs.paginationNumberBtnsContainer.append(...pageBtns);
+      } else {
+        start = pageBtns.slice(0, 3);
+        finish = pageBtns.slice(pageBtns.length - 3, pageBtns.length);
+
+        const moreBtn = document.createElement('button');
+        moreBtn.textContent = '...';
+        moreBtn.setAttribute('disabled', '');
+        moreBtn.classList.add('pagination-button-item');
+
+        refs.paginationNumberBtnsContainer.append(...start, moreBtn, ...finish);
+        break;
+      }
+    }
+  }
+
   function onPaginationBtnClick(evt) {
-    evt.preventDefault();
-
     const toSearch = document.getElementById('search');
     toSearch.scrollIntoView({ behavior: 'smooth' }, true);
 
@@ -91,41 +113,31 @@ export function renderPagination(cocktailArr) {
 
     refs.mainCocktailsGallery.innerHTML = '';
 
-    // if (evt.target.nodeName !== 'BUTTON') {
-    //   return;
-    // }
-
-    if (btnValue === 'leftPag') {
-      currentPageIndex -= 1;
-      if (currentPageIndex < 0) {
-        currentPageIndex = 0;
-      }
-      cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
+    if (refs.mainCocktailsText.textContent !== 'Searching results') {
+      refs.mainCocktailsText.textContent = 'Searching results';
     }
+    switch (btnValue) {
+      case 'leftPag':
+        {
+          currentPageIndex -= 1;
+          if (currentPageIndex < 0) {
+            currentPageIndex = 0;
+          }
+          cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
+        }
+        break;
 
-    if (btnValue === 'rightPag') {
-      currentPageIndex += 1;
-      if (currentPageIndex > totalPagesNum - 1) {
-        currentPageIndex = totalPagesNum - 1;
-      }
-      cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
+      case 'rightPag':
+        {
+          currentPageIndex += 1;
+          cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
+        }
+        break;
     }
 
     if (!isNaN(Number(btnValue))) {
       currentPageIndex = Number(btnValue - 1);
       cocktailMainCardRender(sortedCardsArr[currentPageIndex]);
-      moreBtnIndex = currentPageIndex + 1;
-    }
-
-    if (currentPageIndex === 0) {
-      refs.leftPagBtn[0].classList.add('is-hidden');
-    } else {
-      refs.leftPagBtn[0].classList.remove('is-hidden');
-    }
-    if (currentPageIndex === totalPagesNum - 1) {
-      refs.rightPagBtn[0].classList.add('is-hidden');
-    } else {
-      refs.rightPagBtn[0].classList.remove('is-hidden');
     }
     arrowCheck();
   }
