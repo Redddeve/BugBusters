1a132f3fd2f7cf72b7fff8aa42330003f8baed7c
Redddeve <popko.dmitry797@gmail.com>
2023-09-04 08:27:36 +0300

    Revert "Merge pull request #90 from Redddeve/features/kulik"
    This reverts commit 6254ce957b2abd100cce7599a9a092458235acab, reversing
    changes made to 3f1a14cd02a2d9b98acf47b8e87e3132ce5f677a.

--- src/js/cocktail-fav-card-render.js
+++ src/js/cocktail-fav-card-render.js
@@ -103,7 +103,7 @@ function cocktailMainCardTemplate(
             </button>
             <button 
               type="button"
-              class="delete-btn card-cocktail-delete-icon-use deleteButtonn is-hidden"
+              class="delete-btn card-cocktail-delete-icon-use deleteButtonn is-hidden "
               data-id="${cocktailID}"
             >
   <svg
